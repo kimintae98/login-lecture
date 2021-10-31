c0848099db5ba1380621df4589db60cc77d77961
kimintae <kit98@naver.com>
2021-10-31 22:50:51 +0900

    벡엔드에서 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,43 @@
 "use strict";
 
-const home =  (req,res) => {
-    res.render("home/index");
-}
+const { request } = require("../../../app");
 
-const login = (req,res) => {
-    res.render("home/login");
-}
+const output = {
+    home:  (req,res) => {
+        res.render("home/index");
+    },
+    
+    login: (req,res) => {
+        res.render("home/login");
+    },
+};
+
+const users = {
+    id: ["kit1998", "kit1234"],
+    psword: ["1234", "123"],
+};
+
+const process = {
+    login: (req,res) => {
+        const id = req.body.id,
+            psword = req.body.psword;
+
+        if (users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if (users.psword[idx] === psword){
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하셨습니다.",
+        });
+    },
+}; 
 
 module.exports = {
-    home,
-    login,
+    output,
+    process,
 }; 
