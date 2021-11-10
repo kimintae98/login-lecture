9c8ee62ab8514386c81938afcec8f39a3426990f
kimintae <kit98@naver.com>
2021-11-10 17:45:51 +0900

    벡엔드 맛보기 최적화

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -9,14 +9,14 @@ class User {
     async login() {
         const client = this.body;
         try{
-        const { id, psword } = await UserStorage.getUsersInfo(client.id);
-        if(id){
-            if (id === client.id && psword === client.psword) {
+        const user = await UserStorage.getUsersInfo(client.id);
+        if(user){
+            if (user.id === client.id && user.psword === client.psword) {
                 return { success: true };
             }
             return { success: false, msg: "비밀번호가 틀렸습니다." };
         }
-        return { success: false, msg: "존재하지 않는 아이다입니다." };
+            return { success: false, msg: "존재하지 않는 아이디입니다." };
         } catch (err) {
             return { success: false , err};
         }
